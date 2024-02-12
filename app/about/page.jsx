390f89ef12ec37aebe21eb95637c23d3c8bb3b16
Thomas C <t.corbieres@gmail.com>
2024-02-12 18:08:05 +0100

    Ajustement du responsive page about + Ajout animation star homepage

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -8,8 +8,8 @@ const About = () => {
         <div className="flex pb-20">
           <h1 className="h1 leading-none">À propos</h1>
         </div>
-        <div className="flex flex-col-reverse md:flex-row items-start justify-between">
-          <div className="wrapper-text max-w-[640px]">
+        <div className="flex flex-col-reverse lg:flex-row items-start justify-between gap-10">
+          <div className="wrapper-text max-w-full lg:max-w-[50%]">
             <p>
               Lorem ipsum dolor sit amet consectetur. Tellus tortor mauris
               commodo et faucibus velit id eget. Venenatis et tempus metus morbi
@@ -25,6 +25,29 @@ const About = () => {
               fusce velit sapien etiam eget.
             </p>
             <ul className="flex">
+              <IconMagnetic>
+                <li className="p-5">
+                  <a href="https://www.linkedin.com/in/thomas-corbieres/" target="_blank">
+                    <svg
+                      height="32"
+                      viewBox="0 0 72 72"
+                      width="32"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g fill="none" fill-rule="evenodd">
+                        <path
+                          d="M8,72 L64,72 C68.418278,72 72,68.418278 72,64 L72,8 C72,3.581722 68.418278,-8.11624501e-16 64,0 L8,0 C3.581722,8.11624501e-16 -5.41083001e-16,3.581722 0,8 L0,64 C5.41083001e-16,68.418278 3.581722,72 8,72 Z"
+                          fill="#FFF"
+                        />
+                        <path
+                          d="M62,62 L51.315625,62 L51.315625,43.8021149 C51.315625,38.8127542 49.4197917,36.0245323 45.4707031,36.0245323 C41.1746094,36.0245323 38.9300781,38.9261103 38.9300781,43.8021149 L38.9300781,62 L28.6333333,62 L28.6333333,27.3333333 L38.9300781,27.3333333 L38.9300781,32.0029283 C38.9300781,32.0029283 42.0260417,26.2742151 49.3825521,26.2742151 C56.7356771,26.2742151 62,30.7644705 62,40.051212 L62,62 Z M16.349349,22.7940133 C12.8420573,22.7940133 10,19.9296567 10,16.3970067 C10,12.8643566 12.8420573,10 16.349349,10 C19.8566406,10 22.6970052,12.8643566 22.6970052,16.3970067 C22.6970052,19.9296567 19.8566406,22.7940133 16.349349,22.7940133 Z M11.0325521,62 L21.769401,62 L21.769401,27.3333333 L11.0325521,27.3333333 L11.0325521,62 Z"
+                          fill="#110F52"
+                        />
+                      </g>
+                    </svg>
+                  </a>
+                </li>
+              </IconMagnetic>
               <IconMagnetic>
                 <li className="p-5">
                   <a href="https://github.com/ausma-tc" target="_blank">
@@ -115,13 +138,12 @@ const About = () => {
               </IconMagnetic>
             </ul>
           </div>
-          <div className="wrapper-img mb-10 md:mb-0 max-w-[100%] md:max-w-[600px] overflow-hidden">
+          <div className="wrapper-img mb-10 md:mb-0 max-w-full lg:max-w-[40%] overflow-hidden">
             <Image
               src="/images/Thomas_C_B&W_2023.png"
               width={600}
               height={1280}
               alt="Picture of the author"
-              className="md:max-w-2xl"
             />
           </div>
         </div>
--- src/components/ui/heroSection.jsx
+++ src/components/ui/heroSection.jsx
@@ -1,10 +1,15 @@
+import StarAnim from "./StarAnim";
+
 const HeroSection = () => {
   return (
     <section className="font-amphora py-20 md:py-32 px-6 md:px-20">
-      <h1 className="uppercase text-[15vw] leading-[100%] min-w-[90%] pb-20">
-        Thomas <br />
-        <span className="font-switzer font-bold">Corbières</span>
-      </h1>
+      <div className=" w-fit">
+        <h1 className="uppercase relative text-[15vw] leading-[100%] min-w-[90%] pb-20">
+          Thomas <br />
+          <span className="font-switzer font-bold">Corbières</span>
+        <StarAnim />
+        </h1>
+      </div>
       <div className="flex w-full justify-end">
         <p className="max-w-lg">
           Hey! Je m’appelle Thomas, développeur front-end de 28 ans.
