2ac3f5a0022b754fefbe69a5a1568b35468bfdcd
Thomas C <t.corbieres@gmail.com>
2024-02-19 22:50:40 +0100

    Suppression page loading

--- app/loadinssg.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export default function App({ Component, pageProps, router }) {
-  return (
-    <main className="mainContent">
-      <h1 className="h1">Loading...</h1>
-    </main>
-  );
-}
